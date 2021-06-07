1d6cca5555f4f5612bcf87d9c2171da7c70f868d
dddushesss <37773701+dddushesss@users.noreply.github.com>
2021-06-07 15:36:47 +0300

    Task 1

---
+++ Assets/Scripts/Model/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using View;
+
+namespace Model
+{
+    public class Enemy : Unit
+    {
+        public override void Move()
+        {
+            //TODO implement
+            throw new System.NotImplementedException();
+        }
+
+        public override void Hit()
+        {
+            //TODO implement
+            throw new System.NotImplementedException();
+        }
+    }
+}
